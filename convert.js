11203a22b73008164a13f090f17623afe3b2f4e5
Jeremy Symon <jtsymon@gmail.com>
2015-09-25 11:04:11 +1200

    Made source less shit

---
+++ convert.js
@@ -0,0 +1,30 @@
+function convert(text) {
+    text = text.toUpperCase();
+    var root = document.getElementById("root");
+    root.innerHTML = "";
+    for (var i = 0, n = text.length; i < n; i++) {
+        var c = text[i];
+        if (c === ' ') {
+            root.innerHTML += '<br>';
+        } else {
+            var img = window.characters[c];
+            if (!img) {
+                alert("Invalid character '" + c +"'");
+                return;
+            }
+            root.appendChild(img.cloneNode());
+        }
+    }
+}
+window.onload = function() {
+    window.characters = [];
+    window.waiting_for = 26;
+    for (var i = 0; i < 26; i++) {
+        var c = String.fromCharCode(65 + i);
+        window.characters[c] = new Image();
+        window.characters[c].onload = function() {
+            window.waiting_for --;
+        }
+        window.characters[c].src = c + ".png";
+    }
+}
